da7da31f1c308e3a4a209020ae053d3a39f7730e
David Winer <david@convex.dev>
2026-09-10 10:30:54 -0700

    Add cron and preference extraction API modules

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,9 +10,11 @@
 
 import type * as auth from "../auth.js";
 import type * as chat from "../chat.js";
+import type * as crons from "../crons.js";
 import type * as http from "../http.js";
 import type * as lib_musicAgent from "../lib/musicAgent.js";
 import type * as lib_preferenceAuth from "../lib/preferenceAuth.js";
+import type * as lib_preferenceExtraction from "../lib/preferenceExtraction.js";
 import type * as lib_preferenceTools from "../lib/preferenceTools.js";
 import type * as lib_preferenceTypes from "../lib/preferenceTypes.js";
 import type * as preferenceWorkflows from "../preferenceWorkflows.js";
@@ -28,9 +30,11 @@ import type {
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   chat: typeof chat;
+  crons: typeof crons;
   http: typeof http;
   "lib/musicAgent": typeof lib_musicAgent;
   "lib/preferenceAuth": typeof lib_preferenceAuth;
+  "lib/preferenceExtraction": typeof lib_preferenceExtraction;
   "lib/preferenceTools": typeof lib_preferenceTools;
   "lib/preferenceTypes": typeof lib_preferenceTypes;
   preferenceWorkflows: typeof preferenceWorkflows;
